8f320bbe92c423cd5546ad27d5e0da016849c8e6
mushtariyibodillayeva <dilfuzaxamrayeva53@gmail.com>
2022-11-22 22:32:35 +0500

    new y

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const containerEl = document.querySelector(".container");
+
 window.addEventListener("mousemove", (event) => {
   containerEl.innerHTML = `
     <div class="mouse-event">
     ${event.clientX}
     <h4>Mouse X o'qi(px)</h4>
+    </div>
+    <div class="mouse-event">
+    ${event.clientY}
+    <h4>Mouse Y O'qi(px)</h4>
     </div>`;
 });
